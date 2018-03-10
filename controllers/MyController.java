2e7dfa3d5a02d50b1c0ec6f4a4ae0bd37d6fac20
souro89 <souro89@gmail.com>
2018-03-10 19:51:44 +0400

    Dependency Injection

---
+++ controllers/MyController.java
@@ -0,0 +1,13 @@
+package com.datakettle.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String hello(){
+
+        System.out.println("Hello World!!");
+        return "foo";
+    }
+}
